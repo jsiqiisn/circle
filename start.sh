a6305905d09e28b2f98d487d252167672ff29352
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-05-22 13:16:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-TOKEN="7fd05cb6ce15d49eef7fb7204eb0e3e4f7ace99ed8cb63b5d3" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="f788fbd93b36d16928d01ba09d789d968e5f3c1df2361a2811" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
